9aea21d5e099659ccd3f3d6434f79abd6a122b36
Romain Petit <contact@romainpetit.com>
2018-02-25 17:49:22 +0100

    Use Zepto instead of jQuery

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,47 @@
 // document.addEventListener('keydown', function(event) {
 //     if (event.ctrlKey) {
-//         // open helgp widget
+//         // Toggle the background grid display
 //           var layout = document.getElementById('layout')
 //           if (layout.classList.contains('off')) {
-//             // The box that we clicked has a class of bad so let's remove it and add the good class
 //            layout.classList.remove('off');
 //            layout.classList.add('on');
 //           } else {
-//             // The user obviously can't follow instructions so let's alert them of what is supposed to happen next
 //             layout.classList.remove('on');
 //             layout.classList.add('off');
 //           }
 //     }
 // });
+
+// Adds fadeOut and fadeIn to Zepto
+$(document).ready(function() {
+    (function($) {
+        $.extend($.fn, {
+            fadeIn: function(ms) {
+                if (typeof(ms) === 'undefined') ms = 500;
+
+                $(this).css({
+                    display: 'block',
+                    opacity: 0
+                }).animate({
+                    opacity: 1
+                }, ms);
+
+                return this;
+            },
+
+            fadeOut: function(ms) {
+                if (typeof(ms) === 'undefined') ms = 500;
+
+                $(this).css({
+                    opacity: 1
+                }).animate({
+                    opacity: 0
+                }, ms, 'linear', function() {
+                    $(this).css('display', 'none');
+                });
+
+                return this;
+            }
+        })
+    })(Zepto)
+});
